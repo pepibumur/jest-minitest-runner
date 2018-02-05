a54a851e9e1eed2c9d23ae21531d9d69b89fd17e
Pedro Piñera <pepibumur@gmail.com>
2018-02-05 08:15:35 -0500

    Some fixes

--- example/test/example_test.rb
+++ example/test/example_test.rb
@@ -1,15 +1,5 @@
 require "test_helper"
 
 class ExampleTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    # refute_nil ::Example::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert true
-  end
-
-  def test_shakira
-    assert false
-  end
+  
 end
