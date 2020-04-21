979dfb3adf7a004c50dc70371a694718e18ef37d
Eloy Durán <eloy.de.enige@gmail.com>
2020-04-21 17:22:10 +0200

    [build] Update latest artefacts

--- build/index.js
+++ build/index.js
@@ -1,3 +1,6 @@
 "use strict";
-const minitest_runner_1 = require("./minitest-runner");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const minitest_runner_1 = __importDefault(require("./minitest-runner"));
 module.exports = minitest_runner_1.default;
--- build/minitest-runner.js
+++ build/minitest-runner.js
@@ -1,17 +1,28 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const Child = require("child_process");
-const path = require("path");
-const throat = require("throat");
-const output_parser_1 = require("./output-parser");
+const Child = __importStar(require("child_process"));
+const path = __importStar(require("path"));
+const throat_1 = __importDefault(require("throat"));
+const output_parser_1 = __importDefault(require("./output-parser"));
 class CancelRun extends Error {
     constructor(message) {
         super(message);
@@ -19,25 +30,33 @@ class CancelRun extends Error {
     }
 }
 exports.CancelRun = CancelRun;
-class MinitestRunner {
-    constructor(globalConfig, outputParser = new output_parser_1.default()) {
+/**
+ * Jumping through hoops to:
+ * - Type check the constructor according to upstream's TestRunner class:
+ *   https://www.typescriptlang.org/docs/handbook/interfaces.html#class-types
+ * - Type check the implementation of the class has all of the upstream methods.
+ */
+const MinitestRunner = class MinitestRunner {
+    constructor(globalConfig, context, outputParser) {
         this.globalConfig = globalConfig;
+        this.context = context;
         this.outputParser = outputParser;
+        this.outputParser = outputParser || new output_parser_1.default();
     }
-    runTests(tests, watcher, onStart, onResult, onFailure, options) {
+    runTests(tests, watcher, onStart, onResult, onFailure, _options) {
         return __awaiter(this, void 0, void 0, function* () {
-            const mutex = throat(this.globalConfig.maxWorkers);
-            return Promise.all(tests.map(test => {
+            const mutex = throat_1.default(this.globalConfig.maxWorkers);
+            return Promise.all(tests.map((test) => {
                 return mutex(() => __awaiter(this, void 0, void 0, function* () {
                     if (watcher.isInterrupted()) {
                         throw new CancelRun();
                     }
                     yield onStart(test);
                     return this.runTest(test)
-                        .then(result => {
+                        .then((result) => {
                         onResult(test, result);
                     })
-                        .catch(error => onFailure(test, error));
+                        .catch((error) => onFailure(test, error));
                 }));
             }));
         });
@@ -50,14 +69,14 @@ class MinitestRunner {
                 const testPath = path.join(this.globalConfig.rootDir, "test");
                 const child = Child.spawn("ruby", [`-I"${libPath}:${testPath}"`, `"${test.path}"`], {
                     cwd: this.globalConfig.rootDir,
-                    shell: true
+                    shell: true,
                 });
                 let stdout = "";
                 child.stdout.setEncoding("utf-8");
-                child.stdout.on("data", data => {
+                child.stdout.on("data", (data) => {
                     stdout += data;
                 });
-                child.stdout.on("error", error => {
+                child.stdout.on("error", (error) => {
                     reject(error);
                 });
                 child.stdout.on("close", () => {
@@ -74,5 +93,5 @@ class MinitestRunner {
             });
         });
     }
-}
+};
 exports.default = MinitestRunner;
